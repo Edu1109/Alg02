fea03d2fe87bf7d8de41733f954660ee39d99ffb
Edu1109 <vilcayaurisebastian@gmail.com>
2026-03-20 17:13:03 -0500

    Update temperatura_controller.py

--- app/controllers/temperatura_controller.py
+++ app/controllers/temperatura_controller.py
@@ -30,5 +30,5 @@ def obtener_datos():
 
     datos = [{"id": r[0], "valor": r[1]} for r in rows]
 
-    return {"datos": datos, "user_name" : "edu@usmp.pe"}
-    return {"datos": datos, "password" : "charmander01"}
+    return {"datos": datos, "user_name" : "edu@usmp.pe","password" : "charmander01"}
+
